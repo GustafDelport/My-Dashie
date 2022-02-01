aa7e63527705089d49b85c098e691f133f71bb81
Gustaf Delport <gustafdelport@gmail.com>
2022-02-01 11:54:24 +0200

    done up until deleting todo next is to edit

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Todo } from '../shared/todo.model';
 import { TodoService } from '../shared/todo.service';
 
@@ -11,7 +12,7 @@ export class TodosComponent implements OnInit {
 
   todos: Todo[];
   
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService, private router: Router) { }
 
   ngOnInit(): void {
     this.todos = this.todoService.getTodos();
@@ -20,6 +21,14 @@ export class TodosComponent implements OnInit {
   toggleCompleted(todo: Todo){
     //sets the completed status to what ever it is currently not
     this.todoService.updateTodo(todo.id,{completed: !todo.completed})
+  }
+
+  onEditClick(todo: Todo){
+    this.router.navigate(['/todos',todo.id]);
+  }
+
+  onDeleteClick(todo: Todo){
+    this.todoService.deleteTodo(todo.id);
   }
 
 }
